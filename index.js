333a91e6a5395cee5e9ed9c2d92f90fd5811d98f
NoorFatima1818 <159902924+NoorFatima1818@users.noreply.github.com>
2024-05-27 20:57:04 -0700

    This is my first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const currency = {
+    USD: 1,
+    EUR: 0.94,
+    CNY: 7.24,
+    JPY: 153.28,
+    GBP: 0.80,
+    CAD: 1.38,
+    INR: 83.61,
+    PKR: 277.30
+};
+async function convertCurrency() {
+    console.log(chalk.bold.blue("\n=== Currency Converter ===\n"));
+    try {
+        let user_answer = await inquirer.prompt([
+            {
+                name: "from",
+                message: chalk.cyan("Enter currency you want to convert from?"),
+                type: "list",
+                choices: ["USD", "EUR", "CNY", "JPY", "GBP", "CAD", "INR", "PKR"]
+            },
+            {
+                name: "to",
+                message: chalk.cyan("Enter currency you want to convert to?"),
+                type: "list",
+                choices: ["USD", "EUR", "CNY", "JPY", "GBP", "CAD", "INR", "PKR"]
+            },
+            {
+                name: "amount",
+                message: chalk.cyan("Enter your amount:"),
+                type: "number",
+                validate: (value) => {
+                    if (isNaN(value) || value <= 0) {
+                        return "Please enter a valid amount greater than zero.";
+                    }
+                    return true;
+                }
+            }
+        ]);
+        let fromAmount = currency[user_answer.from];
+        let toAmount = currency[user_answer.to];
+        let amount = user_answer.amount;
+        let baseAmount = amount / fromAmount;
+        let convertedAmount = baseAmount * toAmount;
+        console.log(chalk.green(`Your converted amount is ${convertedAmount.toFixed(2)}.`));
+    }
+    catch (error) {
+        console.error(chalk.red("An error occurred during the currency conversion."));
+        console.error(error);
+    }
+}
+convertCurrency();
